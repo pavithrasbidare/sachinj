0e7b340e9a94eafad5197a909bc52fd20676bc9f
sachinbm <sachinbm40@gmail.com>
2021-12-18 11:26:05 +0000

    test1

---
+++ 5_or_not.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter the integer values for num1"
+read num1
+if [ $num1 -eq 5 ]
+	then 
+		echo "$num is equal to 5"
+else
+echo "$num1 is is not 5"
+fi
---
+++ add.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Enter the integer values for num1"
+read num1
+echo "Enter the integer values for num1"
+read num2
+sum=`expr $num1 + $num2`
+echo "The sum of two numbers is $sum"
