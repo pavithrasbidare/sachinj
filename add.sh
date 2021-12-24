d38c14a820fbff0f9bea699a3af628b546a2c60f
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2021-12-24 15:12:04 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Enter the integer values for num1"
 read num1
+echo "$num1"
 echo "Enter the integer values for num1"
 read num2
+echo "$num2"
 sum=`expr $num1 + $num2`
 echo "The sum of two numbers is $sum"
